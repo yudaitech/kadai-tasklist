59157dec6acb3b2d96eb142f69bd9ad277085fb4
yudai.ito <tourokusenyou1324@yahoo.co.jp>
2019-03-02 06:34:37 +0000

    calamn kadai

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190301084627) do
+ActiveRecord::Schema.define(version: 20190302060730) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20190301084627) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
